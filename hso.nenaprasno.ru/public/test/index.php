460a144bc6bede227a0a2d495238f3ee8014fe86
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-05-03 18:21:20 +0700

    HSO: 2 stage closed

--- hso.nenaprasno.ru/public/test/index.php
+++ hso.nenaprasno.ru/public/test/index.php
@@ -8,30 +8,7 @@ $APPLICATION->SetTitle("Главная");
         <? if (! $USER->IsAuthorized()): ?>
             <? include(__DIR__ . '/templates/login.php'); ?>
         <? else: ?>
-            <? $userData = getTestInfo($USER->GetID()); ?>
-            <? if ($userData['accepted']): ?>
-                <?
-                    $testInfo = getTest($userData);
-                    if (($testInfo == null) && array_key_exists('begin', $_REQUEST)){
-                        $testInfo = beginTest($userData);
-                    }
-                ?>
-
-                <? if ($testInfo['begin']): ?>
-                    <? if (checkTestTime($testInfo)): ?>
-                        <? include(__DIR__ . '/templates/test.php'); ?>
-                    <? else: ?>
-                        <? include(__DIR__ . '/templates/testTimeIsOver.php'); ?>
-                    <? endif ?>
-
-                <? else: ?>
-                    <? include(__DIR__ . '/templates/beginTest.php'); ?>
-                <? endif ?>
-                
-            <? else: ?>
-
-                <? include(__DIR__ . '/templates/orderNotAccepted.php'); ?>
-            <? endif ?>
+            <? include(__DIR__ . '/templates/testClosed.php'); ?>
         <? endif ?>
     </div>
 </main>
